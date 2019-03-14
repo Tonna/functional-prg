4af932ec6720c655f471c79e8c598fc2051dceca
Anton Yakovchuk <yakovchuk.a.p@gmail.com>
2019-03-14 15:28:39 +0200

    week3 hw

--- elm-study-group/week3/src/Homework.elm
+++ elm-study-group/week3/src/Homework.elm
@@ -1,4 +1,4 @@
-module Homework exposing (maybeToList, updateList)
+module Homework exposing (find, isJust, maybeToList, updateList, updateListKv)
 
 
 maybeToList : Maybe a -> List a
@@ -27,3 +27,50 @@ updateList shouldChange f xs =
                         h :: aaa t
     in
     aaa xs
+
+
+find : (a -> Bool) -> List a -> Maybe a
+find f xss =
+    let
+        aaa list =
+            case list of
+                [] ->
+                    Nothing
+
+                h :: tail ->
+                    Just h
+    in
+    aaa (List.filter f xss)
+
+
+isJust : Maybe a -> Bool
+isJust x =
+    case x of
+        Nothing ->
+            False
+
+        Just a ->
+            True
+
+
+updateListKv : List ( k, v ) -> k -> (v -> Maybe v) -> List ( k, v )
+updateListKv old k f =
+    let
+        aaa list =
+            case list of
+                [] ->
+                    []
+
+                x :: xs ->
+                    if Tuple.first x == k then
+                        case f (Tuple.second x) of
+                            Nothing ->
+                                aaa xs
+
+                            Just a ->
+                                ( Tuple.first x, a ) :: aaa xs
+
+                    else
+                        x :: aaa xs
+    in
+    aaa old
